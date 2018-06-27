943cdf0906090a75ed77b3d74bff4d1bcf3043ac
SMVictor <victor.salvatierra@outlook.com>
2018-06-27 17:34:16 -0600

    Avance del tab de venues
    Se agregó volley para hacer el request del API de foursquare y se agregó un recycler view

---
+++ app/src/main/java/com/practice/project/android_bootcamp/VenueAdapter.java
@@ -0,0 +1,58 @@
+package com.practice.project.android_bootcamp;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class VenueAdapter extends RecyclerView.Adapter<VenueAdapter.VenueAdapterViewHolder> {
+
+    private List<String> mVenueData;
+
+    public VenueAdapter() {
+    }
+
+    /**
+     * Cache of the children views for a venue list item.
+     */
+    public class VenueAdapterViewHolder extends RecyclerView.ViewHolder {
+
+        public final TextView mVenueTextView;
+
+        public VenueAdapterViewHolder(View view) {
+            super(view);
+            mVenueTextView = (TextView) view.findViewById(R.id.tv_venue_data);
+        }
+    }
+    @Override
+    public VenueAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.venue_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        return new VenueAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(VenueAdapterViewHolder venueAdapterViewHolder, int position) {
+        String venueForThisPosition = mVenueData.get(position);
+        venueAdapterViewHolder.mVenueTextView.setText(venueForThisPosition);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (null == mVenueData) return 0;
+        return mVenueData.size();
+    }
+
+    public void setVenueData(List<String> venueData) {
+        mVenueData = venueData;
+        notifyDataSetChanged();
+    }
+}
